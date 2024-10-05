b9778f366ca3c3bbd8a41a66f4d3067a5c8a0b21
Shehzan Khan <ershehzankhan@gmail.com>
2024-10-05 19:07:03 +0530

    Add files via upload

--- PatternPrinting.cpp
+++ PatternPrinting.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     int row, col;
     for (row = 1; row <= n; row++)
     {
-        for (col = n; col >= 1; col--)
+        for (col = 1; col <= n; col++)
         {
-            cout << col << " ";
-        }cout << endl;
+            cout << col * col << " ";
+        }
+        cout << endl;
     }
-    
 }
