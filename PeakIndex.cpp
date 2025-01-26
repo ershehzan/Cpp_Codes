433c9f21e62e9c6e5e5c29e3f0ef416427e0a718
Shehzan Khan <ershehzankhan@gmail.com>
2025-01-26 13:39:04 +0530

    Update PeakIndex.cpp

--- PeakIndex.cpp
+++ PeakIndex.cpp
@@ -1,37 +1,36 @@
-// Lc 852
-
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    // int n;
-    // cout << "Enter the Element to search : ";
-    // cin >> n;
-
     int arr[] = {2, 4, 6, 8, 10, 8, 5};
-    // int size = sizeof(arr) / sizeof(arr[0]);
-    int start = 0, end = 6;
-    while (start <= end)
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int start = 0, end = size - 1;
+
+    while (start < end)
     {
+        int mid = start + (end - start) / 2;
 
-        int mid = end + (start - end) / 2;
-
+        // Check if mid is the peak element
         if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
         {
-            cout << "Peak Element is : " << mid << endl;
-
+            cout << "Peak Element is at index: " << mid << endl;
             return 0;
         }
 
-        else if (arr[mid] > arr[mid - 1])
+        // Move to the side of the larger neighbor
+        if (arr[mid] < arr[mid + 1])
         {
-            start = mid++;
+            start = mid + 1;
         }
         else
         {
-            end = mid--;
+            end = mid - 1;
         }
     }
-    cout << "Element is not present in the array" << endl;
+
+    // After the loop, start and end converge at the peak element
+    cout << "Peak Element is at index: " << start << endl;
+
     return 0;
 }
