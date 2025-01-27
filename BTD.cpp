995145fe48135a2dd2b41870091530dab19d2793
Shehzan Khan <ershehzankhan@gmail.com>
2025-01-27 12:31:27 +0530

    Update BTD.cpp

--- BTD.cpp
+++ BTD.cpp
@@ -1,18 +1,18 @@
-// binary to decimal
+// Binary to Decimal
 #include <iostream>
 using namespace std;
 int main()
 {
-    int n;
+    int num;
     int rem, ans = 0, mul = 1;
-    cout << "Enter a binary number : ";
-    cin >> n;
-    while (n > 0)
+    cout << "Enter a Binary number : ";
+    cin >> num;
+    while (num> 0)
     {
         // remainder
-        rem = n % 10;
+        rem = num % 10;
         // qoutient
-        n /= 10;
+        num /= 10;
         // answer
         ans += rem * mul;
         // multiple
