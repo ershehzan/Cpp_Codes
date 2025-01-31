5134fa1af5e0883a9696a77c04586b82d2f7e9a7
Shehzan Khan <ershehzankhan@gmail.com>
2025-01-31 11:41:04 +0530

    Update Function.cpp

--- Function.cpp
+++ Function.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 using namespace std;
 
-bool prime(int n)
+bool prime(int num)
 {
-    if (n == 2)
+    if (num == 2)
         return 0;
-    for (int i = 2; i < n; i++)
+    for (int i = 2; i < num; i++)
     {
-        if (n % i == 0)
+        if (num % i == 0)
             return 0;
     }
     return 1;
 }
 
-int fact(int n)
+int fact(int num)
 {
 
     double fact = 1;
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= num; i++)
     {
         fact *= i;
     }
