8505d3c79401969616aa5a29c1d4c9bb881db17a
Shehzan Khan <ershehzankhan@gmail.com>
2025-02-01 18:02:06 +0530

    Update PatternQ3.cpp

--- PatternQ3.cpp
+++ PatternQ3.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 int main()
 {
-    // i=row    j=column
-    int n, i, j;
+ 
+    int n, row, col;
     cout << "Enter a number :";
     cin >> n;
-    for (i = 1; i <= n; i++)
+    for (row = 1; row <= n; row++)
     {
         // Nested loop
-        for (j = 1; j <= n; j++)
+        for ( col = 1;  col <= n;  col++)
         {
-            cout << j << " ";
+            cout <<  col << " ";
         }
         cout << endl;
     }
