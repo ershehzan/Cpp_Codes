9b3ef18f13aa13991f7c85cc8686bd4466bab734
Shehzan Khan <ershehzankhan@gmail.com>
2025-02-04 12:11:12 +0530

    Update Max-E-arr.cpp

--- Max-E-arr.cpp
+++ Max-E-arr.cpp
@@ -3,16 +3,25 @@
 using namespace std;
 int main()
 {
-    int arr[10] = {2, 4, 5, 6, 7, 8, 9, 1, 56, 11};
-    int ans = INT_MAX;
-    for (int i = 0; i < 10; i++)
+    int arr[1000];
+    int n;
+    cout << "Enter the index:";
+    cin >> n;
+    cout << "Enter the Elements: ";
+    for (int i = 0; i < n; i++)
     {
-        // MIN VALUE
-        if (arr[i] < ans)
+
+        cin >> arr[i];
+    }
+    int ans = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        // MAX   VALUE
+        if (arr[i] > ans)
             ans = arr[i];
     }
 
-    cout << ans;
+    cout <<"The MAX Element in Array is : "<< ans;
 
     return 0;
 }
