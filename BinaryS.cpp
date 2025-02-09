9255b6edb0d2cc86e70aa307ed22c9a90ac1d620
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-09 12:44:25 +0530

    Update BinaryS.cpp

--- BinaryS.cpp
+++ BinaryS.cpp
@@ -1,45 +1,58 @@
 #include <iostream>
 using namespace std;
 
+// Function to perform binary search on a sorted array
 int BinarySearch(int arr[], int target, int num)
 {
-    int start = 0, end = num- 1, mid;
+    int start = 0, end = num - 1, mid;
+    
     while (start <= end)
     {
-        int mid = (start + end) / 2;
-        if (arr[mid] == target)
+        int mid = (start + end) / 2; // Calculate middle index
+        
+        if (arr[mid] == target) // If the target is found
         {
             cout << "Element found at index ";
-            return mid;
+            return mid; // Return index of the found element
         }
-        else if (arr[mid] <target)
+        else if (arr[mid] < target) // If target is greater, ignore left half
         {
-
             start = mid + 1;
         }
-        else
+        else // If target is smaller, ignore right half
         {
-
             end = mid - 1;
         }
     }
-    return -1;
+    return -1; // Return -1 if the element is not found
 }
 
 int main()
 {
-    int arr[1000];
+    int arr[1000]; // Declare an array with a maximum size of 1000
     int num;
-    cout << "Enter the index of array : ";
+    
+    // Take input for array size
+    cout << "Enter the number of elements in the array: ";
     cin >> num;
-    cout << "Enter the elements of array : ";
-    for (int i = 0; i <num; i++)
+    
+    // Take input for array elements
+    cout << "Enter the elements of the sorted array: ";
+    for (int i = 0; i < num; i++)
         cin >> arr[i];
 
     int target;
-    cout << "Enter the number to search : " << endl;
+    
+    // Take input for the target element to search
+    cout << "Enter the number to search: ";
     cin >> target;
 
-    cout << BinarySearch(arr, target,num) << endl;
-    return 0;
+    // Perform binary search and print the result
+    int result = BinarySearch(arr, target, num);
+    if (result != -1)
+        cout << result << endl;
+    else
+        cout << "Element not found" << endl;
+
+    return 0; // Exit program
 }
