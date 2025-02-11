9c04b48a76e5a7be6f34ab6c68b5c83a8bca4612
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-11 11:44:08 +0530

    Update BTD.cpp

--- BTD.cpp
+++ BTD.cpp
@@ -1,23 +1,26 @@
-// Binary to Decimal
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    int num;
-    int rem, ans = 0, mul = 1;
-    cout << "Enter a Binary number : ";
+    int num; // Variable to store the binary number input
+    int rem, ans = 0, mul = 1; // rem: remainder, ans: decimal equivalent, mul: place value multiplier
+
+    // Input: Get the binary number from the user
+    cout << "Enter a Binary number: ";
     cin >> num;
-    while (num> 0)
+
+    // Convert binary to decimal
+    while (num > 0)
     {
-        // remainder
-        rem = num % 10;
-        // qoutient
-        num /= 10;
-        // answer
-        ans += rem * mul;
-        // multiple
-        mul *= 2;
+        rem = num % 10; // Extract the last digit (remainder)
+        num /= 10;      // Remove the last digit (quotient)
+        ans += rem * mul; // Convert and add the value to the decimal result
+        mul *= 2;       // Update the multiplier (power of 2)
     }
-    cout << "Decimal number is : " << ans;
+
+    // Output the decimal equivalent
+    cout << "Decimal number is: " << ans;
+
     return 0;
 }
