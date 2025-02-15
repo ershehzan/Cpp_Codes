66a40537f821b21d4e1b74c09eef0df528a6de79
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-15 17:26:47 +0530

    Update maxMin.cpp

--- maxMin.cpp
+++ maxMin.cpp
@@ -1,32 +1,33 @@
-#include <iostream>
-#include <climits>
-using namespace std;
+#include <iostream> // Include the standard input-output stream library
+#include <climits> // Include library for INT_MAX and INT_MIN constants
+using namespace std; // Use the standard namespace
 
 int main()
 {
-int arr[5] = {13, 24, 35, 40, 51};
+    int arr[5] = {13, 24, 35, 40, 51}; // Declare and initialize an array with 5 elements
+    
+    int i; // Loop variable
+    int ans = INT_MAX; // Initialize ans with the maximum possible integer value
+    
+    // Finding the minimum value in the array
+    for (i = 0; i < 5; i++)
+    {
+        if (arr[i] < ans) // If the current element is smaller than ans
+            ans = arr[i]; // Update ans with the smaller value
+    }
+    cout << "Min value is : ";
+    cout << ans << endl; // Print the minimum value
+    
+    // Finding the maximum value in the array
+    ans = INT_MIN; // Reinitialize ans with the minimum possible integer value
+    
+    for (i = 0; i < 5; i++)
+    {
+        if (arr[i] > ans) // If the current element is greater than ans
+            ans = arr[i]; // Update ans with the greater value
+    }
+    cout << "Max value is : ";
+    cout << ans << endl; // Print the maximum value
 
-int i;
-int ans = INT_MAX;
-// Min value
-for (i = 0; i < 5; i++)
-{
-    if (arr[i] < ans)
-        ans = arr[i];
-}
-cout << "Min value is : ";
-cout << ans << endl;
-
-// Max value
-ans = INT_MIN;
-
-for (i = 0; i < 5; i++)
-{
-    if (arr[i] > ans)
-        ans = arr[i];
-}
-cout << "Max value is : ";
-cout << ans << endl;
-
-return 0;
+    return 0; // Return 0 to indicate successful execution
 }
