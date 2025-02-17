6efdad134cefccd23551a180c4f135b61bc86585
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-17 13:03:38 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -3,12 +3,24 @@ using namespace std;
 int main()
 {
 
-    int arr[8] = {35, 33, 42, 10, 14, 19, 27, 44};
+ int n; // Declare variables: 'n' for array size, 'i' and 'j' for loops, 'key' for sorting
+    int arr[1000];    // Declare an array of size 1000 to store user input
 
-    for (int i = 0; i < 7; i++)
+    // Prompt the user to enter the number of elements in the array
+    cout << "Enter the Index: ";
+    cin >> n; // Read the number of elements from user input
+
+    // Prompt the user to enter the array elements
+    cout << "Enter the elements of array: ";
+    for (i = 0; i < n; i++)
+    {
+        cin >> arr[i]; // Read each element and store it in the array
+    }
+
+    for (int i = 0; i < n-1; i++)
     {
 
-        for (int j = 0; j < 8 - i - 1; j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
@@ -16,7 +28,7 @@ int main()
             }
         }
     }
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
