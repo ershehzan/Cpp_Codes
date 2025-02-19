c9bcf335004d07c969fa28debc6414041174571d
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-19 18:06:26 +0530

    Update Semicolon.cpp

--- Semicolon.cpp
+++ Semicolon.cpp
@@ -1,10 +1,15 @@
-#include<iostream>
-using namespace std;
-int main (){
-if(cout<<"Hello World!"<<endl){
+#include <iostream> 
+using namespace std;  
+int main() {
+    // This statement prints "Hello World!" to the console
+    if (cout << "Hello World!" << endl) {
+        // The if condition here is redundant as cout always returns a valid stream
+    }
 
-}
-switch(cout<<C++ !",0){
+    // Corrected switch statement with proper syntax
+    switch (cout << "C++ !" << endl, 0) {  
+        // The comma operator evaluates 'cout << "C++ !"' first, then uses '0' in switch
+        // Since 'switch' has no cases, it does nothing
+    }
 
-}
 }
