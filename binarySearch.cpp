ece372b03fd49b0e1f877a5104bd77761bc47e20
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-19 18:11:51 +0530

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,57 +1,56 @@
-// Binary search
 #include <iostream>
 using namespace std;
 
 int main()
 {
-
+    // Variable declarations
     int n, l = 0, m, target;
+    int arr[1000]; // Array to store user input (max size 1000)
 
-    int arr[1000];
-
-    cout << "Enter the index: ";
+    // Take the size of the array from the user
+    cout << "Enter the number of elements in the array: ";
     cin >> n;
-    cout << "Enter the Elements of Array: ";
 
+    // Input elements of the array
+    cout << "Enter " << n << " elements (array should be sorted for binary search to work correctly): ";
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
-    cout << "Enter the Number to search: ";
-
+    // Take the target number to search
+    cout << "Enter the number to search: ";
     cin >> target;
-    // Set r to the last index of the array
+
+    // Set 'r' to the last index of the array
+    int r = n - 1;
 
     // Begin binary search
-    int r = n - 1;
     while (l <= r)
     {
+        // Calculate the middle index
+        m = l + (r - l) / 2; // Prevents potential integer overflow
 
-        m = l + (r - l) / 2; //  calculation of the mid index
-
+        // If the middle element is the target, print the index and exit
         if (arr[m] == target)
         {
-
             cout << "Element is present at index: " << m << endl;
-
-            return 0; // Exit if element is found
+            return 0; // Exit the program
         }
-
+        // If target is greater than the middle element, search in the right half
         else if (arr[m] < target)
         {
-
-            l = m + 1; // Move to the right half
+            l = m + 1;
         }
-
+        // If target is smaller than the middle element, search in the left half
         else
         {
-
-            r = m - 1; // Move to the left half
+            r = m - 1;
         }
     }
 
-    cout << "Element is not present in the array: " << endl; // If element is not found
+    // If the element is not found, print the message
+    cout << "Element is not present in the array." << endl;
 
     return 0;
 }
