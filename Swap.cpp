1ad269a7902a8771e5c480c55e030da189cad874
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-20 10:07:22 +0530

    Update Swap.cpp

--- Swap.cpp
+++ Swap.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 using namespace std;
 
-// void swap(int a, int b) // pass by value
-
-void swap(int &a, int &b) // pass by reference
+// Function to swap two numbers using pass by reference
+void swap(int &a, int &b) 
 {
-
-    int c;
+    int c; // Temporary variable to store the value of 'a'
     c = a;
     a = b;
     b = c;
@@ -15,9 +13,16 @@ void swap(int &a, int &b) // pass by reference
 int main()
 {
     int x, y;
+    
+    // Prompting the user to enter values for x and y
     cout << "Enter the value of a and b : ";
-    cin >> x >> y;
+    cin >> x >> y; // Taking input from the user
+
+    // Calling the swap function to interchange the values of x and y
     swap(x, y);
+
+    // Displaying the swapped values
     cout << x << " " << y;
+    
     return 0;
 }
