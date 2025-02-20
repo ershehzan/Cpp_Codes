41cc458661cea5d738be97b0be7c076d45ae6ab1
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-20 10:08:53 +0530

    Update Switch.cpp

--- Switch.cpp
+++ Switch.cpp
@@ -1,35 +1,41 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-
     int days;
-    cout << "Enter a Day number : ";
+
+    // Prompt user for input
+    cout << "Enter a Day number (1-7): ";
     cin >> days;
 
+    // Switch statement to match input number with corresponding weekday
     switch (days)
     {
     case 1:
-        cout << "Monday";
+        cout << "Monday" << endl;
         break;
     case 2:
-        cout << "Tuesday";
+        cout << "Tuesday" << endl;
         break;
     case 3:
-        cout << "Wednesday";
+        cout << "Wednesday" << endl;
         break;
     case 4:
-        cout << "Thursday";
+        cout << "Thursday" << endl;
         break;
     case 5:
-        cout << "Friday";
+        cout << "Friday" << endl;
         break;
     case 6:
-        cout << "Saturday";
+        cout << "Saturday" << endl;
         break;
     case 7:
-        cout << "Sunday";
+        cout << "Sunday" << endl;
         break;
+    default:
+        // Handles cases where input is not between 1-7
+        cout << "Invalid input! Please enter a number between 1 and 7." << endl;
     }
 
     return 0;
