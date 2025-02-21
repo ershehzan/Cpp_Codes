b73235275e177ec07da7572d56a4137a96d4f9e4
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-21 10:55:36 +0530

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -5,8 +5,8 @@ using namespace std;
 // Main function where execution starts
 int main()
 {
-    int n, i, j, key; // Declare variables: 'n' for array size, 'i' and 'j' for loops, 'key' for sorting
-    int arr[1000];    // Declare an array of size 1000 to store user input
+    int n, key; // Declare variables: 'n' for array size, 'key' for sorting
+    int arr[1000]; // Declare an array of size 1000 to store user input
 
     // Prompt the user to enter the number of elements in the array
     cout << "Enter the Index: ";
@@ -14,32 +14,32 @@ int main()
 
     // Prompt the user to enter the array elements
     cout << "Enter the elements of array: ";
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         cin >> arr[i]; // Read each element and store it in the array
     }
 
     // Insertion Sort Algorithm
-    for (i = 1; i < n; i++) // Start from the second element (index 1) to the last
+    for (int i = 1; i < n; i++) // Start from the second element (index 1) to the last
     {
         key = arr[i]; // Store the current element (key) to be placed correctly
-        j = i - 1;    // Start comparing with the previous element
+        int j = i - 1; // Start comparing with the previous element
 
         // Shift elements that are greater than 'key' one position ahead
         while (j >= 0 && arr[j] > key)
         {
             arr[j + 1] = arr[j]; // Move the larger element one step forward
-            j = j - 1;           // Move backward to compare previous elements
+            j = j - 1; // Move backward to compare previous elements
         }
         arr[j + 1] = key; // Place 'key' in its correct position
     }
 
     // Print the sorted array
     cout << "Sorted Array is: ";
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " "; // Output each sorted element separated by a space
     }
 
-    return 0;
+    return 0; // Program ends successfully
 }
