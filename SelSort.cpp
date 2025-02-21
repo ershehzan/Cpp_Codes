d0c4c8e7f477d5c751dbe11203c2c2e2263d34c4
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-21 10:48:40 +0530

    Update SelectionSort.cpp

--- SelSort.cpp
+++ SelSort.cpp
@@ -4,20 +4,28 @@ using namespace std;
 int main()
 {
 
-    int arr[6] = {10, 5, 6, 7, 8, 1};
+    int arr[1000];
+    int n;
+    cout<<"Enter the index: ";
+    cin>>n;
+    cout<<"Enter the elements of array";
+    for(int i = 0;i<n;i++){
+cin>>arr[i];
 
-    for (int i = 0; i < 5; i++)
+    }
+
+    for (int i = 0; i < n-1; i++)
     {
 
         int index = i;
-        for (int j = i + 1; j < 6; j++)
+        for (int j = i + 1; j < n; j++)
         {
             if (arr[j] < arr[index])
                 index = j;
         }
         swap(arr[i], arr[index]);
     }
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < n; i++)
     {
         cout << arr[i] <<" ";
     }
