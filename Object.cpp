98a8a0a6ad4fb8dd25433f20e8d0fb5e36697fc6
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-23 11:58:03 +0530

    Update Object.cpp

--- Object.cpp
+++ Object.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 using namespace std;
+
+// Class definition for Car
 class Car {
 public:
-string color;
-string brand;
-void drive() {
-cout << "The " << color << " " << brand << " is driving." << endl;
-}
+    string color;  // Stores the car's color
+    string brand;  // Stores the car's brand
+
+    // Method to simulate driving the car
+    void drive() {
+        cout << "The " << color << " " << brand << " is driving." << endl;
+    }
 };
+
 int main() {
-Car myCar;
-myCar.color = "Red";
-myCar.brand = "Toyota";
-myCar.drive();
-return 0;
+    // Creating an object of the Car class
+    Car myCar;
+
+    // Setting properties of the car
+    myCar.color = "Red";
+    myCar.brand = "Toyota";
+
+    // Calling the drive method
+    myCar.drive();
+
+    return 0; // Indicate successful program execution
 }
