dca129a91135955efce3e6e39dda2fd9053065f5
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-24 10:01:36 +0530

    Update DTB.cpp

--- DTB.cpp
+++ DTB.cpp
@@ -1,24 +1,27 @@
-// Decimal to binary
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    int num;
-    int rem, ans = 0, mul = 1;
+    int num;       // Variable to store the decimal number
+    int rem;       // To store the remainder when dividing by 2
+    int ans = 0;   // To store the binary equivalent
+    int mul = 1;   // Multiplier to place the binary digit at the correct position
 
+    // Input from the user
     cout << "Enter a Decimal Number : ";
     cin >> num;
+
+    // Loop to convert decimal to binary
     while (num > 0)
     {
-        // remainder
-        rem = num % 2;
-        // Qoutient
-        num /= 2;
-        // answer
-        ans += rem * mul;
-        // multiple
-        mul *= 10;
+        rem = num % 2;  // Get remainder (0 or 1)
+        num /= 2;       // Divide number by 2 for the next iteration
+        ans += rem * mul; // Add remainder at the correct position
+        mul *= 10;      // Update the position multiplier (units, tens, hundreds, etc.)
     }
+
+    // Output the binary equivalent
     cout << "Binary Number is : " << ans;
-    return 0;
+    return 0; // End of the program
 }
