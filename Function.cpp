4318ad5ebb38f015213647c30dddd67ad04b6fb4
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-24 10:04:18 +0530

    Update Function.cpp

--- Function.cpp
+++ Function.cpp
@@ -1,38 +1,53 @@
 #include <iostream>
 using namespace std;
 
+// Function to check if a number is prime
 bool prime(int num)
 {
-    if (num == 2)
-        return 0;
-    for (int i = 2; i < num; i++)
+    if (num <= 1)  // Numbers less than or equal to 1 are not prime
+        return false;
+    if (num == 2)  // 2 is a prime number
+        return true;
+    for (int i = 2; i <= num / 2; i++)  // Loop to check divisibility up to num/2
     {
-        if (num % i == 0)
-            return 0;
+        if (num % i == 0)  // If divisible, not a prime
+            return false;
     }
-    return 1;
+    return true;  // If no divisors, it's prime
 }
 
+// Function to calculate factorial of a number
 int fact(int num)
 {
-
-    double fact = 1;
+    int fact = 1;  // Initialize factorial as 1
     for (int i = 1; i <= num; i++)
     {
-        fact *= i;
+        fact *= i;  // Multiply the current number with the result
     }
-    return fact;
+    return fact;  // Return the computed factorial
 }
+
 int main()
 {
-    int a, b;
+    int a, b;  // Variables to store user input
+
+    // Input from the user
     cout << "Enter the first number: ";
     cin >> a;
     cout << "Enter the second number: ";
     cin >> b;
-    cout << "Prime of a is : " << prime(a) << endl;
-    cout << "Factorial of b is : " << fact(b) << endl;
-    cout << "Prime of b-a is : " << prime(b - a) << endl;
-    cout << "Factorial of b-a is : " << fact(b - a) << endl;
-    return 0;
+
+    // Check and display if 'a' is prime
+    cout << "Is " << a << " a prime number? : " << (prime(a) ? "Yes" : "No") << endl;
+
+    // Calculate and display factorial of 'b'
+    cout << "Factorial of " << b << " is : " << fact(b) << endl;
+
+    // Check and display if (b - a) is prime
+    cout << "Is " << (b - a) << " a prime number? : " << (prime(b - a) ? "Yes" : "No") << endl;
+
+    // Calculate and display factorial of (b - a)
+    cout << "Factorial of " << (b - a) << " is : " << fact(b - a) << endl;
+
+    return 0;  
 }
