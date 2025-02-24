f2593f6be54c283227c14c97cd4809a384eabd3e
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-24 10:02:18 +0530

    Update Even.cpp

--- Even.cpp
+++ Even.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-
-    int num;
+    int num;  // Variable to store the user input
     cout << "Enter a number :";
-    cin >> num;
-    int i = 1;     // Initialize
-    while (i <= num) // Break
-    {
+    cin >> num;  // Taking input from the user
 
+    int i = 1;  // Initialize loop counter
+
+    // Loop to iterate from 1 to the entered number
+    while (i <= num) // Loop will run until 'i' is less than or equal to 'num'
+    {
+        // Check if the current number is even
         if (i % 2 == 0)
         {
-            cout << i << " ";
+            cout << i << " ";  // Print even number
         }
-        i++; // Update
+        i++; // Increment the counter (Update step)
     }
-    return 0;
+
+    return 0; 
 }
