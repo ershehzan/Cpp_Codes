14fc3b8ce7d6821ba5607f9ee0804414b44586fa
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-02-25 17:59:09 +0530

    Update SumofOdd.cpp

--- SumofOdd.cpp
+++ SumofOdd.cpp
@@ -1,20 +1,24 @@
-#include <iostream>
-using namespace std;
+#include <iostream> 
+using namespace std; 
 int main()
 {
-    int n, sum = 0;
+    int n, sum = 0; // Declare variables: n for input, sum to store the sum of odd numbers
 
+    // Prompt the user to enter the number of terms
     cout << "Enter the number of terms: ";
-    cin >> n;
+    cin >> n; // Read input value from the user
 
-    for (int i = 1; i <= n; i++)
+    // Loop from 1 to n to find the sum of the first n odd numbers
+    for (int i = 1; i <= n * 2; i++) // Loop runs until it covers at least n odd numbers
     {
-        if (i % 2 != 0)
+        if (i % 2 != 0) // Check if the number is odd
         {
-            sum += i;
+            sum += i; // Add the odd number to the sum
         }
     }
-    cout << "The sum of first " << n << " odd numbers is: " << sum;
 
-    return 0;
+    // Output the result
+    cout << "The sum of the first " << n << " odd numbers is: " << sum;
+
+    return 0; 
 }
