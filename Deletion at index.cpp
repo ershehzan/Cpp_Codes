58dbef54df81acb486c49cb6c08a5dfa5aeb3220
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-01 19:33:02 +0530

    Add files via upload

--- Deletion at index.cpp
+++ Deletion at index.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void deleteElement(int arr[], int &size, int index) {
+    if (index < 0 || index >= size) {
+        cout << "Invalid index!" << endl;
+        return;
+    }
+    for (int i = index; i < size - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    size--; // Reduce array size
+}
+
 int main() {
     int arr[] = {10, 20, 30, 40, 50};
     int size = sizeof(arr) / sizeof(arr[0]);
@@ -9,15 +20,7 @@ int main() {
     cout << "Enter the index to delete: ";
     cin >> index;
 
-    if (index < 0 || index >= size) {
-        cout << "Invalid index!" << endl;
-        return 1;
-    }
-    
-    for (int i = index; i < size - 1; i++) {
-        arr[i] = arr[i + 1];
-    }
-    size--; // Reduce array size
+    deleteElement(arr, size, index);
 
     cout << "Updated array: ";
     for (int i = 0; i < size; i++) {
