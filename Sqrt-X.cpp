3d6071835ddd22d8657553b5f0f296797ee6b2bd
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-02 11:57:18 +0530

    Update Sqrt-X.cpp

--- Sqrt-X.cpp
+++ Sqrt-X.cpp
@@ -1,35 +1,34 @@
-//Leet code problem 69 square root of X
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Solution {
 public:
-
     int mySqrt(int x) {
-        //solution start from here
-        int start=0,end=x,mid,ans;
-while(start<=end){
-    mid=start+(end-start)/2;
-    if(mid*mid==x){
-        return mid;
-
-
-    }
-    else if(mid*mid<x)
-    {
-        ans=mid;
-        start=mid+1;
-    }
-    else{
-        end=mid;
-    }
-
-}
-return ans;
-//end here
+        // Solution starts here
+        int start = 0, end = x, mid, ans;
+        
+        // Implementing binary search to find the square root
+        while (start <= end) {
+            mid = start + (end - start) / 2; // Prevents integer overflow
+            
+            if (mid * mid == x) {
+                return mid; // If mid squared is exactly x, return mid
+            }
+            else if (mid * mid < x) {
+                ans = mid; // Store the potential floor value of sqrt(x)
+                start = mid + 1; // Move to the right half
+            }
+            else {
+                end = mid - 1; // Move to the left half
+            }
+        }
+        
+        return ans; // Return the integer part of the square root
+        // End here
     }
 };
-int main(){
-    Solution s;
+
+int main() {
+    Solution s; // Creating an instance of the Solution class
+    return 0;
 }
