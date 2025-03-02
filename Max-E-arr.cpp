d97e870526ab759fe7f1e56d890f1cd54c6d12bd
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-02 11:27:29 +0530

    Update Max-E-arr.cpp

--- Max-E-arr.cpp
+++ Max-E-arr.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <climits>
 using namespace std;
+
 int main()
 {
-    int arr[1000];
-    int n;
-    cout << "Enter the index:";
+    int arr[1000]; // Array to store elements
+    int n; // Variable to store number of elements
+    
+    cout << "Enter the index: ";
     cin >> n;
+    
     cout << "Enter the Elements: ";
     for (int i = 0; i < n; i++)
     {
-
-        cin >> arr[i];
+        cin >> arr[i]; // Taking input for array elements
     }
-    int ans = INT_MIN;
+    
+    int ans = INT_MIN; // Initializing with the smallest possible integer value
+    
+    // Finding the maximum value in the array
     for (int i = 0; i < n; i++)
     {
-        // MAX   VALUE
         if (arr[i] > ans)
-            ans = arr[i];
+            ans = arr[i]; // Updating max value if current element is greater
     }
 
-    cout <<"The MAX Element in Array is : "<< ans;
+    cout << "The MAX Element in Array is : " << ans; // Displaying the result
 
     return 0;
 }
