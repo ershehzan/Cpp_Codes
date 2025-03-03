a9ffab60dd6b81af45802c977a44fc32947acae8
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-03 10:43:13 +0530

    Update LC-69.cpp

--- LC-69.cpp
+++ LC-69.cpp
@@ -1,35 +1,34 @@
-//Leet code problem 69 square root of X
-
 #include<iostream>
 using namespace std;
 
 class Solution {
 public:
-
     int mySqrt(int x) {
-        //solution start from here
-        int start=0,end=x,mid,ans;
-while(start<=end){
-    mid=start+(end-start)/2;
-    if(mid*mid==x){
-        return mid;
-
-
-    }
-    else if(mid*mid<x)
-    {
-        ans=mid;
-        start=mid+1;
-    }
-    else{
-        end=mid;
-    }
-
-}
-return ans;
-//end here
+        // Binary search approach to find the square root of x
+        int start = 0, end = x, mid, ans;
+        
+        while (start <= end) {
+            mid = start + (end - start) / 2; // Avoids overflow
+            
+            // If mid squared is exactly x, return mid
+            if (mid * mid == x) {
+                return mid;
+            }
+            // If mid squared is less than x, store mid as potential answer and move right
+            else if (mid * mid < x) {
+                ans = mid;
+                start = mid + 1;
+            }
+            // If mid squared is greater than x, move left
+            else {
+                end = mid - 1;
+            }
+        }
+        
+        return ans; // Return the integer part of the square root
     }
 };
-int main(){
+
+int main() {
     Solution s;
 }
