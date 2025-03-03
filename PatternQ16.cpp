a06ccae0bdd7615b9ab465f0759158d09fd203ac
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-03 10:45:15 +0530

    Update PatternQ16.cpp

--- PatternQ16.cpp
+++ PatternQ16.cpp
@@ -4,22 +4,30 @@ using namespace std;
 int main()
 {
     int i, j, n;
+    
+    // Taking input from the user
     cout << "Enter the number : ";
     cin >> n;
+    
+    // Outer loop to control the rows
     for (i = 1; i <= n; i++)
     {
         // Inner loop to print decreasing numbers in each row
         for (j = 1; j <= n - (i - 1); j++)
         {
-            cout << i << " ";
+            cout << i << " "; // Print the current row number
         }
-        cout << endl; // Move to the next line after printing stars for the current row
+        cout << endl; // Move to the next line after printing numbers for the current row
     }
+    
     return 0;
 }
-/* Output
+
+/* Output Example:
+Enter the number : 5
 1 1 1 1 1 
 2 2 2 2 
 3 3 3 
 4 4 
-5   */
+5 
+*/
