dd356873e2b36276ffb48b6022b075b725f5c5c8
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-06 11:34:37 +0530

    Update Function overloading.cpp

--- Function overloading.cpp
+++ Function overloading.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 using namespace std;
 
+// Function overloading example
+// Two functions with the same name but different parameter types.
+
+// Function to add two integers
 int add(int a, int b)
 {
-    return a + b;
+    return a + b; // Returns the sum of two integers
 }
 
+// Function to add two doubles
+// Overloaded function with different parameter type
 double add(double a, double b)
 {
-    return a + b;
+    return a + b; // Returns the sum of two doubles
 }
 
 int main()
 {
-    cout<<"Function overloading: "<<endl;
-    cout<<"Integer addition: ";
-    cout << add(1, 2) << endl;
-    cout<<"Double addition: ";
-    cout << add(1.1, 2.2) << endl;
-}
+    cout << "Function overloading: " << endl;
+    
+    // Calling the integer addition function
+    cout << "Integer addition: ";
+    cout << add(1, 2) << endl; // Calls the int version of add()
+    
+    // Calling the double addition function
+    cout << "Double addition: ";
+    cout << add(1.1, 2.2) << endl; // Calls the double version of add()
+    
+    return 0;
