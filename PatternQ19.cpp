51abdce33c58558c84088a0a6902bee520e6852d
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-08 11:01:14 +0530

    Update PatternQ19.cpp

--- PatternQ19.cpp
+++ PatternQ19.cpp
@@ -1,26 +1,35 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
+
 int main()
 {
-    int n, i, j;
-    cout << "Enter the number : ";
-    cin >> n;
+    int n, i, j; // Declare variables
 
+    // Prompt the user to enter a number
+    cout << "Enter the number: ";
+    cin >> n; // Take input from the user
+
+    // Outer loop: Controls the number of rows (1 to n)
     for (i = 1; i <= n; i++)
     {
-    //space print
-        for (j = 1; j <= n-i; j++)
+        // First inner loop: Prints spaces for right alignment
+        for (j = 1; j <= n - i; j++)
         {
-            cout << "  ";
+            cout << "  "; // Print two spaces
         }
+
+        // Second inner loop: Prints numbers in decreasing order from 'i' to 1
         for (j = i; j >= 1; j--)
         {
-            cout << j << " ";
+            cout << j << " "; // Print number followed by a space
         }
-        cout << endl;
+
+        cout << endl; // Move to the next line after printing each row
     }
-    return 0;
+
+    return 0; 
 }
+
 /* Output
 
         1 
