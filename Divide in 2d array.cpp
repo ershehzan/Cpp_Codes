61b0e7d9c8a4cbc370d3f1a63e78bd79eda7b6a4
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-13 11:20:06 +0530

    Update Divide in 2d array.cpp

--- Divide in 2d array.cpp
+++ Divide in 2d array.cpp
@@ -13,22 +13,29 @@ int main()
                       4, 5, 6,
                       7, 8, 9};
 
+    // Resultant array to store element-wise division
     int ans[3][3];
 
+    // Performing element-wise division of arr1 by arr2
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            ans[i][j] = arr1[i][j] / arr2[i][j];
+            if (arr2[i][j] != 0) // Avoid division by zero
+                ans[i][j] = arr1[i][j] / arr2[i][j];
+            else
+                ans[i][j] = 0; // Assign 0 if division by zero occurs
         }
     }
 
+    // Printing the resultant array
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
             cout << ans[i][j] << " ";
         }
+        cout << endl; // Print new line for better readability
     }
 
     return 0;
