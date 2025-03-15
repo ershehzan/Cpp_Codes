837764f26763cb7b28a86d7321954c0541b37aca
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-15 12:00:15 +0530

    Update PatternQ25.cpp

--- PatternQ25.cpp
+++ PatternQ25.cpp
@@ -1,33 +1,39 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    // i=row   j= column
+    // i = row, j = column
     int n, i, j;
-    cout << "Enter the number : ";
+    cout << "Enter the number of rows: ";
     cin >> n;
 
     for (i = 1; i <= n; i++)
     {
-        // space print
+        // Print spaces for right alignment
         for (j = 1; j <= n - i; j++)
+        {
+            cout << "  "; // Two spaces for proper formatting
+        }
 
-            cout << "  ";
-        // star print
+        // Print numbers in increasing order
         for (j = 1; j <= i; j++)
         {
-
             cout << j << " ";
         }
-        cout << endl;
+
+        cout << endl; // Move to the next line after each row
     }
+
     return 0;
 }
-/* Output
+
+/* Output for n = 5
 
         1 
       1 2 
     1 2 3 
   1 2 3 4 
 1 2 3 4 5 
-   */
+
+*/
