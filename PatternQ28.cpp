39e5270db4cb9611c02bcd0cee6543b95edfd1e8
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-16 11:59:32 +0530

    Update PatternQ28.cpp

--- PatternQ28.cpp
+++ PatternQ28.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    // i=row   j= column
+    // i = row index, j = column index
     int n, i, j;
-    cout << "Enter the number : ";
+    cout << "Enter the number: ";
     cin >> n;
 
+    // Loop for each row (inverted pyramid)
     for (i = n; i >= 1; i--)
     {
-        // space print
+        // Printing leading spaces
         for (j = 1; j <= n - i; j++)
+            cout << "  "; // Two spaces for proper alignment
 
-            cout << "  ";
-        // star print
+        // Printing stars in decreasing order
         for (j = 1; j <= 2 * i - 1; j++)
-
             cout << "* ";
 
-        cout << endl;
+        cout << endl; // Move to the next line after printing each row
     }
+
     return 0;
 }
-/* Output
 
- * * * * * * * * *    
+/* Output when n = 5:
+
+* * * * * * * * *    
   * * * * * * *      
     * * * * *        
-      * * * 
-        * 
+      * * *          
+        *            
 
-   */
+*/
