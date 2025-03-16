092657481707f848420d66850d729832dab14e4f
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-16 11:58:31 +0530

    Update Wave form.cpp

--- Wave form.cpp
+++ Wave form.cpp
@@ -2,33 +2,37 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
 int main()
 {
-
+    // Defining a 2D vector (matrix) with 3 rows and 4 columns
     vector<vector<int>> matrix = {
         {1, 2, 3, 4},
         {5, 6, 7, 8},
         {9, 10, 11, 12}};
 
-    cout << "wave form" << endl;
+    cout << "Wave Form Traversal:" << endl;
 
+    // Iterating through each column
     for (int j = 0; j < 4; j++)
     {
+        // If the column index is even, traverse top to bottom
         if (j % 2 == 0)
         {
             for (int i = 0; i < 3; i++)
             {
                 cout << matrix[i][j] << " ";
             }
-          
         }
-            else
+        // If the column index is odd, traverse bottom to top
+        else
+        {
+            for (int i = 2; i >= 0; i--)
             {
-                for(int i=2;i>=0;i--){
-                    cout<<matrix[i][j]<<" ";
-                }
+                cout << matrix[i][j] << " ";
+            }
         }
-        
-}
-return 0;
+    }
+    
+    return 0;
 }
