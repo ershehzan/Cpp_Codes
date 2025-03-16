50f309dc76e2e0bcc04878c9be71556e659aaacd
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-16 12:00:26 +0530

    Update PatternQ29.cpp

--- PatternQ29.cpp
+++ PatternQ29.cpp
@@ -1,47 +1,52 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    // i=row   j= column
+    // i = row index, j = column index
     int n, i, j;
-    cout << "Enter the number : ";
+    cout << "Enter the number: ";
     cin >> n;
-    // opp loop from n to 1
+
+    // Upper half of the pattern (from n to 1)
     for (i = n; i >= 1; i--)
     {
-        // * print
+        // Printing left side stars
         for (j = 1; j <= i; j++)
             cout << "* ";
 
-        // space print
+        // Printing spaces in the middle
         for (j = 1; j <= 2 * n - 2 * i; j++)
-            cout << "  ";
+            cout << "  "; // Two spaces for proper alignment
 
-        // * print
+        // Printing right side stars
         for (j = 1; j <= i; j++)
             cout << "* ";
 
-        cout << endl;
+        cout << endl; // Move to the next line after printing each row
     }
-    // opp loop from 1 to n
+
+    // Lower half of the pattern (from 1 to n)
     for (i = 1; i <= n; i++)
     {
-        // * print
+        // Printing left side stars
         for (j = 1; j <= i; j++)
             cout << "* ";
 
-        // space print
+        // Printing spaces in the middle
         for (j = 1; j <= 2 * n - 2 * i; j++)
-            cout << "  ";
+            cout << "  "; // Two spaces for proper alignment
 
-        // * print
+        // Printing right side stars
         for (j = 1; j <= i; j++)
             cout << "* ";
 
-        cout << endl;
+        cout << endl; // Move to the next line after printing each row
     }
+
     return 0;
 }
+
 /* Output
 
 * * * * * * * * * * * * * * * * 
