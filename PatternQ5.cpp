185da096c3806452f62ae825c0634b9e232ac975
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-17 22:09:10 +0530

    Update PatternQ5.cpp

--- PatternQ5.cpp
+++ PatternQ5.cpp
@@ -1,26 +1,30 @@
-#include <iostream>
-using namespace std;
-int main()
-{
+#include <iostream> 
+using namespace std; 
+int main() {
     // i=row    j=column
-    int n;
-    cout << "Enter a number :";
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
+    int n;  // Declare an integer variable 'n' to store the number of rows
+
+    cout << "Enter a number :";  // Prompt the user to enter a number
+    cin >> n;  // Read the input number and store it in 'n'
+
+    // Loop from 1 to 'n' to iterate over each row
+    for (int i = 1; i <= n; i++) {
         // Nested loop
-        for (int j = 1; j <= n; j++)
-        {
-            cout << " * ";
+        // Loop from 1 to 'n' to print '*' for each column in the current row
+        for (int j = 1; j <= n; j++) {
+            cout << " * ";  // Print '*' followed by a space
         }
-        cout << endl;
+        cout << endl;  // Move to the next line after printing all columns for the current row
     }
 
-    return 0;
+    return 0;  
 }
-/*Output
-5 4 3 2 1 
-5 4 3 2 1 
-5 4 3 2 1 
-5 4 3 2 1 
-5 4 3 2 1  */
+
+/*
+Output Example (for n=5):
+ *  *  *  *  * 
+ *  *  *  *  * 
+ *  *  *  *  * 
+ *  *  *  *  * 
+ *  *  *  *  * 
+*/
