7355245cbe3b0b3e37e6bb3656f7e65b802dcdfc
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-18 11:23:45 +0530

    Update PatternQ7.cpp

--- PatternQ7.cpp
+++ PatternQ7.cpp
@@ -1,29 +1,38 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    // i=row    j=column
+    // Declare an integer variable to store the number of rows and columns
     int num;
 
-    cout << "Enter a number :";
-    cin >> numn;
+    // Prompt the user to enter a number and read the input value
+    cout << "Enter a number: ";
+    cin >> num;
+
+    // Outer loop to handle the number of rows
     for (int i = 1; i <= num; i++)
     {
-      
-        // Nested loop
+        // Inner loop to handle the number of columns
         for (int j = 1; j <= num; j++)
         {
-              char name = 'a' + j - 1;
+            // Calculate the character to print based on the column index
+            char name = 'a' + j - 1;
+            // Print the character followed by two spaces
             cout << name << "  ";
         }
+        // Print a newline character after each row
         cout << endl;
     }
 
+    // Return 0 to indicate successful execution of the program
     return 0;
 }
-/* Output
+
+/* Output example when num = 5:
 a  b  c  d  e        
 a  b  c  d  e        
 a  b  c  d  e        
 a  b  c  d  e        
-a  b  c  d  e   */
+a  b  c  d  e   
+*/
