d049a8541c52a8134a221d20ba955ce6b9fbf20b
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-22 14:08:59 +0530

    Update S-1toN.cpp

--- S-1toN.cpp
+++ S-1toN.cpp
@@ -1,12 +1,14 @@
 // Sum from 1 to n without loop;
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    int n;
-    cout << "Enter a number : ";
-    cin >> n;
-    cout<<"Sum from 1 to "<<n<<" is "<<(n*(n+1)/2);
-
-    return 0;
+    int n; // Declare an integer variable to store the input number
+    cout << "Enter a number : "; // Prompt the user to enter a number
+    cin >> n; // Read the input number from the user
+    // Calculate the sum from 1 to n using the formula n*(n+1)/2
+    cout << "Sum from 1 to " << n << " is " << (n * (n + 1) / 2);
+    
+    return 0; 
 }
