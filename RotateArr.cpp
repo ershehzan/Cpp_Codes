b2a1c955690f7922a36d105bd0278d01f33f2f6d
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-22 14:07:25 +0530

    Update RotateArr.cpp

--- RotateArr.cpp
+++ RotateArr.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-
+    // Declare an array of size 1000
     int arr[1000];
     int n, i;
+
+    // Prompt the user to enter the number of elements
     cout << "Enter the index: ";
     cin >> n;
+
+    // Prompt the user to enter the elements of the array
     cout << "Enter the Elements: ";
     for (i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
+
     // Store the last element in a temporary variable
     int temp = arr[n - 1];
 
@@ -20,14 +26,18 @@ int main()
     {
         arr[i] = arr[i - 1];
     }
+
     // Place the last element at the beginning
     arr[0] = temp;
 
+    // Print the rotated array
     cout << "Rotated array: ";
     for (i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
     cout << endl;
+
+    // Return 0 to indicate successful execution
     return 0;
 }
