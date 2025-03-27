037485e55755ac1bae8b3fe52d0c84c1162ad8a0
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-27 11:22:33 +0530

    Update Factorial.cpp

--- Factorial.cpp
+++ Factorial.cpp
@@ -3,20 +3,20 @@ using namespace std;
 
 int main()
 {
-    int num;  // Variable to store user input
+    int n;  // Variable to store user input
     cout << "Enter a number : ";
-    cin >> num;  // Taking input from the user
+    cin >> n;  // Taking input from the user
 
     double fact = 1;  // Variable to store the factorial result, initialized to 1
 
     // Loop to calculate factorial
-    for (int i = 1; i <= num; i++)
+    for (int i = 1; i <= n; i++)
     {
         fact *= i;  // Multiply the current value of 'fact' by 'i' (fact = fact * i)
     }
 
     // Display the result
-    cout << "Factorial of " << num << " is " << fact;
+    cout << "Factorial of " << n << " is " << fact;
 
     return 0;  
 }
