7ef1a5c465c62a426210906831325b365a85cb61
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-03-28 14:49:02 +0530

    Update Even.cpp

--- Even.cpp
+++ Even.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-    int n;  // Variable to store the user input
+    int num;  // Variable to store the user input
     cout << "Enter a number :";
-    cin >> n;  // Taking input from the user
+    cin >> num;  // Taking input from the user
 
     int i = 1;  // Initialize loop counter
 
     // Loop to iterate from 1 to the entered number
-    while (i <= n) // Loop will run until 'i' is less than or equal to 'num'
+    while (i <= num) // Loop will run until 'i' is less than or equal to 'num'
     {
         // Check if the current number is even
         if (i % 2 == 0)
