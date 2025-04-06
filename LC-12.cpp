3d668b53bd8dc715bdd9d459a29822c3864627a9
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-04-06 15:37:40 +0530

    Update LC-12.cpp

--- LC-12.cpp
+++ LC-12.cpp
@@ -3,9 +3,11 @@
 #include <vector>
 using namespace std;
 
+// Define a class named Solution
 class Solution
 {
 public:
+    // Function to convert an integer to a Roman numeral
     string intToRoman(int num)
     {
         // Define the values of Roman numerals in descending order
@@ -36,14 +38,21 @@ public:
 
 int main()
 {
+    // Create an instance of the Solution class
     Solution solution;
+    // Declare an integer variable to store the input number
     int num;
 
+    // Prompt the user to enter an integer
     cout << "Enter an integer: ";
+    // Read the input number from the user
     cin >> num;
 
+    // Call the intToRoman function and store the result
     string roman = solution.intToRoman(num);
+    // Output the Roman numeral representation of the input number
     cout << "Roman numeral: " << roman << endl;
 
+    
     return 0;
 }
