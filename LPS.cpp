27adfbd683b1194f71caeefd103827ed2acfb55d
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-04-08 20:34:12 +0530

    Update LPS.cpp

--- LPS.cpp
+++ LPS.cpp
@@ -4,18 +4,25 @@
 #include <vector>
 using namespace std;
 
+// Class to provide solutions to specific problems
 class Solution {
 public:
+    // Function to find the length of the longest prefix which is also a suffix
+    // @param s: A reference to the input string
+    // @return: The length of the longest prefix which is also a suffix
     int longestPrefixSuffix(string &s) {
-        vector<int> lps(s.size(), 0);
-        int pre = 0, suf = 1;
+        vector<int> lps(s.size(), 0);  // Vector to store the longest prefix suffix values
+        int pre = 0, suf = 1;  // Pointers to track prefix and suffix positions
 
+        // Iterate through the string to fill the lps array
         while (suf < s.size()) {
+            // If characters match, increment both pointers and update lps array
             if (s[pre] == s[suf]) {
                 lps[suf] = pre + 1;
                 pre++;
                 suf++;
             } else {
+                // If characters do not match and pre is not at the start, update pre using lps array
                 if (pre == 0) {
                     lps[suf] = 0;
                     suf++;
@@ -24,18 +31,21 @@ public:
                 }
             }
         }
+        // Return the last value in lps array which represents the longest prefix which is also a suffix
         return lps[s.size() - 1];
     }
 };
 
 int main() {
-    Solution solution;
-    string input;
+    Solution solution;  // Create an instance of the Solution class
+    string input;  // Variable to store the input string
     cout << "Enter a string: ";
-    cin >> input;
+    cin >> input;  // Read the input string from the user
 
+    // Call the longestPrefixSuffix function and get the result
     int result = solution.longestPrefixSuffix(input);
+    // Print the result
     cout << "The length of the longest prefix which is also a suffix is: " << result << endl;
 
-    return 0;
+    return 0;  
 }
