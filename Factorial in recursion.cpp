84d86f6f458da90746f70f6414b1bf82a9d40ab8
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-04-20 12:14:47 +0530

    Update Factorial in recursion.cpp

--- Factorial in recursion.cpp
+++ Factorial in recursion.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 using namespace std;
 
+// Recursive function to calculate the factorial of a number
+// Parameter:
+// - n: The number for which the factorial is to be calculated
 int fact(int n)
 {
-
-    if (n == 0)
+    if (n == 0) // Base case: The factorial of 0 is 1
     {
         return 1;
     }
-    return n * fact(n - 1);
+    return n * fact(n - 1); // Recursive call to calculate factorial
 }
 
 int main()
 {
+    int n; // Variable to store the user input
+    cout << "Enter a number: "; // Prompt the user to enter a number
+    cin >> n; // Read the input number from the user
 
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    if(n<0){
-        cout<<"Factorial of negative number doesn't exist"<<endl;
-        return 0;
+    if (n < 0) // Check for negative numbers
+    {
+        cout << "Factorial of negative number doesn't exist" << endl; // Display error message
+        return 0; // Exit the program
     }
 
-    cout << "Factorial of " << n << " is:" << fact(n);
-    return 0;
+    // Calculate and display the factorial of the entered number
+    cout << "Factorial of " << n << " is: " << fact(n);
+    return 0; // Return 0 to indicate successful program execution
 }
