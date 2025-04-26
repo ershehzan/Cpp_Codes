2b80cb5ab0ebdedf77a2987310d197e49d6edc5b
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-04-26 19:49:24 +0530

    Update Sum of array elements.cpp

--- Sum of array elements.cpp
+++ Sum of array elements.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
+#include <iostream> // Include the iostream library for input/output operations
 using namespace std;
+
 int main()
 {
+    // Declare and initialize an array with 10 elements
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 11};
 
-
-    int n = 10;
+    int n = 10; // Number of elements in the array
 
     // Variable to store the sum of all elements in the array
     int sum = 0;
@@ -19,5 +20,5 @@ int main()
     // Print the sum of all elements in the array
     cout << "Sum of all elements in the array: " << sum << endl;
 
-    return 0;
+    return 0; // Indicate that the program ended successfully
 }
