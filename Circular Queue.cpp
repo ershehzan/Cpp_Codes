f954973553b3d8d24c630f786f1ca7285df77a6f
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-08 20:25:03 +0530

    Add files via upload

---
+++ Circular Queue.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+
+class Queue
+{
+    int front, rear;
+    int *arr;
+    int size;
+
+public:
+    Queue(int n)
+    {
+        arr = new int[n];
+        size = n;
+        front = rear = -1;
+    }
+
+    bool isEmpty()
+    {
+        return (front == -1);
+    }
+
+    bool isFull()
+    {
+        return ((rear + 1) % size == front);
+    }
+
+    void enqueue(int x)
+    {
+        if (isFull())
+        {
+            cout << "Queue is Full" << endl;
+            return;
+        }
+        else if (isEmpty())
+        {
+            front = rear = 0;
+            arr[rear] = x;
+        }
+        else
+        {
+            rear = (rear + 1) % size;
+            arr[rear] = x;
+        }
+        cout << "Inserting " << x << " in Queue" << endl;
+    }
+
+    void dequeue()
+    {
+        if (isEmpty())
+        {
+            cout << "Queue is Empty" << endl;
+            return;
+        }
+        cout << "Removing " << arr[front] << " from Queue" << endl;
+
+        if (front == rear)
+        {
+            front = rear = -1;
+            cout << "Queue is now Empty" << endl;
+        }
+        else
+        {
+            front = (front + 1) % size;
+        }
+    }
+
+    int start()
+    {
+        if (isEmpty())
+        {
+            cout << "Queue is Empty" << endl;
+            return -1;
+        }
+        cout << "Front element is: " << arr[front] << endl;
+        return arr[front];
+    }
+};
+
+int main()
+{
+    Queue q(5);
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.enqueue(40);
+    q.enqueue(50); // Queue should be full here
+    q.dequeue();
+    q.dequeue();
+    
+
+    cout << q.start() << endl;
+
+    return 0;
+}
