261ee56b43d032f36923c7afefafef64ad7b98c8
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-15 16:11:36 +0530

    Add files via upload

--- Sliding Window.cpp
+++ Sliding Window.cpp
@@ -1,79 +1,43 @@
-//GFG
 #include <iostream>
+#include <queue>
 using namespace std;
 
-// } Driver Code Ends
+void display(queue<int> q)
+{
+    cout << "Sliding Window:";
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+}
 
-class Solution {
-    public:
-        int findSubString(string& s) 
-        {
-                // Initialize the count array to store character frequencies
-                vector<int> count(256, 0);
-                
-                // Initialize pointers and variables
-                int f = 0, sec = 0, len = s.size();
-                int dif = 0;
+int main()
+{
+    int arr[100];
+    int n, k;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Enter the size of the sliding window: ";
+    cin >> k;
 
-                // Count number of unique characters in the string
-                while (f < s.size())
-                {
-                        if (count[s[f]] == 0)
-                                dif++;
-                        count[s[f]]++;
-                        f++;
-                }
+    queue<int> q;
 
-                // Reset count array and pointers for sliding window
-                fill(count.begin(), count.end(), 0);
-                f = 0;
-                sec = 0;
-                int currDif = 0;
-
-                // Iterate over the string with the sliding window approach
-                while (sec < s.size())
-                {
-                        // Expand the window until it contains all unique characters
-                        while (currDif < dif && sec < s.size())
-                        {
-                                if (count[s[sec]] == 0)
-                                        currDif++;
-
-                                count[s[sec]]++;
-                                sec++;
-                        }
-
-                        // Contract the window to find the minimum length substring
-                        while (currDif == dif)
-                        {
-                                len = min(len, sec - f);
-                                count[s[f]]--;
-
-                                if (count[s[f]] == 0)
-                                        currDif--;
-
-                                f++;
-                        }
-                }
-
-                return len; // Return the length of the smallest substring containing all unique characters
-        }
-};
-
-//{ Driver Code Starts.
-// Driver code
-int main() {
-        int t;
-        cin >> t; // Read number of test cases
-        while (t--) {
-
-                string str;
-                cin >> str; // Read the input string
-                Solution ob;
-                cout << ob.findSubString(str) << endl; // Print the result
-
-                cout << "~"
-                         << "\n";
-        }
-        return 0;
+    for (int i = 0; i < k - 1; i++)
+    {
+        q.push(arr[i]);
+    }
+    for (int i = k - 1; i < n; i++)
+    {
+        q.push(arr[i]);
+        display(q);
+        q.pop();
+    }
+    return 0;
 }
