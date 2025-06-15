86eda42eea9d32c9cc1458082c5f7fb225bb8519
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-15 15:29:02 +0530

    Update Queue Using  LL.cpp

--- Queue Using  LL.cpp
+++ Queue Using  LL.cpp
@@ -1,77 +1,92 @@
 #include <iostream>
 using namespace std;
+
+// Node class for singly linked list node
 class Node
 {
 public:
-    int data;
-    Node *next;
+    int data;       // Stores the value of the node
+    Node *next;     // Pointer to the next node in the list
+
+    // Constructor to initialize node with value and set next to NULL
     Node(int val)
     {
         data = val;
         next = NULL;
     }
 };
+
+// Queue class using a singly linked list
 class Queue
 {
-    Node *front;
-    Node *rear;
+    Node *front;    // Pointer to the front node (first element) of the queue
+    Node *rear;     // Pointer to the rear node (last element) of the queue
 
 public:
+    // Constructor initializes an empty queue
     Queue()
     {
         front = rear = NULL;
     }
+
+    // Function to check if queue is empty
     bool isEmpty()
     {
         return front == NULL;
     }
-    // Enqueue operation
+
+    // Enqueue operation: adds a new value at the rear of the queue
     void push(int val)
     {
         if (isEmpty())
         {
-
+            // If queue is empty, create a new node and set both front and rear to it
             front = rear = new Node(val);
-            ;
             cout << "Queue is now initialized with value: " << val << endl;
         }
         else
         {
+            // If queue is not empty, create a new node and link it to the current rear
             Node *temp = new Node(val);
-            rear->next = temp;
+            rear->next = temp;       // Link new node after current rear
             cout << "Value " << val << " added to the queue" << endl;
-            rear = temp;
+            rear = temp;             // Update rear pointer
         }
     }
-    // Dequeue operation
+
+    // Dequeue operation: removes the front element from the queue
     void pop()
     {
         if (isEmpty())
         {
+            // Cannot remove from an empty queue
             cout << "Queue is empty" << endl;
             return;
         }
         else if (front == rear)
         {
+            // Only one element in the queue
             delete front;
             front = rear = NULL;
             cout << "Queue is now empty after pop" << endl;
         }
         else
         {
+            // More than one element: remove the front node
             Node *temp = front;
             cout << "Popped: " << temp->data << endl;
-            front = front->next;
-            delete temp;
+            front = front->next;     // Move front pointer to next node
+            delete temp;             // Free memory of old front node
         }
     }
-    // Peek operation
+
+    // Peek operation: returns the value of the front element without removing it
     int peek()
     {
         if (isEmpty())
         {
             cout << "Queue is empty, no front element" << endl;
-            return -1; // or throw an exception
+            return -1; // or throw an exception if desired
         }
         else
         {
@@ -82,14 +97,16 @@ public:
 
 int main()
 {
+    Queue q;                // Create a queue object
+    q.push(10);             // Add 10 to the queue
+    q.push(20);             // Add 20 to the queue
+    q.push(30);             // Add 30 to the queue
 
-    Queue q;
-    q.push(10);
-    q.push(20);
-    q.push(30);
-    cout << "Front element is: " << q.peek() << endl;
-    q.pop();
-    cout << "Front element after pop is: " << q.peek() << endl;
-    q.pop();
-    cout << "Front element after another pop is: " << q.peek() << endl;
+    cout << "Front element is: " << q.peek() << endl;      // Display current front
+
+    q.pop();                // Remove the front element (10)
+    cout << "Front element after pop is: " << q.peek() << endl; // Display front
+
+    q.pop();                // Remove the front element (20)
+    cout << "Front element after another pop is: " << q.peek() << endl; // Display front
 }
