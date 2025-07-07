0edc05679efeeb75942d960ce847b8ac498c4730
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-07-07 12:20:17 +0530

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -2,16 +2,14 @@
 using namespace std;
 
 // Tree Node
-class Node
-{
+class Node {
 public:
     int data;
     Node *left;
     Node *right;
 
     // Constructor to initialize a new node
-    Node(int val)
-    {
+    Node(int val) {
         data = val;
         left = NULL;
         right = NULL;
@@ -19,11 +17,12 @@ public:
 };
 
 // Insert function to build BST
-Node *insert(Node *root, int val)
-{
+Node* insert(Node* root, int val) {
+    // If current root is NULL, create a new node
     if (!root)
         return new Node(val);
 
+    // Recursively insert in the left or right subtree based on value
     if (val < root->data)
         root->left = insert(root->left, val);
     else
@@ -32,27 +31,31 @@ Node *insert(Node *root, int val)
     return root;
 }
 
-class Solution
-{
+// Solution class containing LCA logic
+class Solution {
 public:
-    Node *LCA(Node *root, Node *n1, Node *n2)
-    {
+    // Function to find Lowest Common Ancestor of two nodes in BST
+    Node* LCA(Node* root, Node* n1, Node* n2) {
         if (!root)
             return NULL;
 
+        // If both nodes are smaller, LCA lies in left subtree
         if (root->data > n1->data && root->data > n2->data)
             return LCA(root->left, n1, n2);
+        
+        // If both nodes are greater, LCA lies in right subtree
         else if (root->data < n1->data && root->data < n2->data)
             return LCA(root->right, n1, n2);
+
+        // Else current node is the split point and hence the LCA
         else
             return root;
     }
 };
 
-int main()
-{
+int main() {
     // Create a sample BST
-    Node *root = NULL;
+    Node* root = NULL;
     root = insert(root, 20);
     insert(root, 8);
     insert(root, 22);
@@ -61,12 +64,14 @@ int main()
     insert(root, 10);
     insert(root, 14);
 
-    Node *n1 = root->left->left;  // Node with value 4
-    Node *n2 = root->left->right; // Node with value 12
+    // Choose two nodes to find LCA of
+    Node* n1 = root->left->left;   // Node with value 4
+    Node* n2 = root->left->right;  // Node with value 12
 
     Solution sol;
-    Node *lca = sol.LCA(root, n1, n2);
+    Node* lca = sol.LCA(root, n1, n2);
 
+    // Print the result
     if (lca)
         cout << "LCA of " << n1->data << " and " << n2->data << " is: " << lca->data << endl;
     else
