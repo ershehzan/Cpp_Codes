8ddb487a80bcfd6a3a75bd48e8f9eeaf7e7b0e83
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-08-16 17:08:41 +0530

    Update 2 Sum.cpp

--- 2 Sum.cpp
+++ 2 Sum.cpp
@@ -3,9 +3,7 @@ using namespace std;
 int main()
 {  // Initialize an array with sorted elements (Two-pointer approach works on sorted arrays)
     int arr[1000] = {3, 4, 6, 8, 11, 12, 34, 41};
-
     int n = 8; // Number of elements in the array
-
     int target; // Variable to store the target sum
     cout << "Enter the target: ";
     cin >> target;
@@ -39,5 +37,6 @@ int main()
     cout << "No pairs found." << endl;
     return 0;
 }
+
 
 
