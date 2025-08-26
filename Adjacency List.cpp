caa22c449d990a7d6c2634f460e1ec1abba43234
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-08-26 10:08:00 +0530

    Update Adjacency List.cpp

--- Adjacency List.cpp
+++ Adjacency List.cpp
@@ -2,8 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 // Undirected Graph using Adjacency Matrix
-int main()
-{
+int main(){
     int vertex, edges;
     cout << "Enter number of vertices and edges: ";
     cin >> vertex >> edges;
@@ -19,8 +18,7 @@ int main()
     }
 
     cout << "Adjacency Matrix:" << endl;
-    for (int i = 0; i < vertex; i++)
-    {
+    for (int i = 0; i < vertex; i++){
         cout<<i<<"->";
         // Print the adjacency list for vertex i
         for (size_t j = 0; j < list[i].size(); j++)
@@ -29,4 +27,5 @@ int main()
         }
         cout << endl;
     }
+
 }
