635648861872267af4199e0e9dd905269a044e7b
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-08-26 10:08:09 +0530

    Update Adjacency List.cpp

--- Adjacency List.cpp
+++ Adjacency List.cpp
@@ -16,7 +16,6 @@ int main(){
         list[u].push_back(v);
         list[v].push_back(u);
     }
-
     cout << "Adjacency Matrix:" << endl;
     for (int i = 0; i < vertex; i++){
         cout<<i<<"->";
@@ -29,3 +28,4 @@ int main(){
     }
 
 }
+
