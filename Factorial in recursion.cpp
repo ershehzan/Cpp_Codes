22128882c7b7e23a6cb63d27576dd736d3299936
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-08-28 18:15:39 +0530

    Update Factorial in recursion.cpp

--- Factorial in recursion.cpp
+++ Factorial in recursion.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 
 int fact(int n){
-    if (n == 0)
-    {
+    if (n == 0) {
         return 1;
     }
     return n * fact(n - 1);
@@ -24,3 +23,4 @@ int main()
     return 0;
 
 }
+
