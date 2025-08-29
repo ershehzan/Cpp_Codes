54f1559bdd542859e267ca6a55f6142dd744bb30
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-08-29 14:21:48 +0530

    Update 2D vector.cpp

--- 2D vector.cpp
+++ 2D vector.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+
 int main(){
     int n,m;
     cout<<"Enter the number of rows and columns";
@@ -33,3 +34,4 @@ for(int i=0;i<n;i++){
 cout<<endl;
 
 }
+
