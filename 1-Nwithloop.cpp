3470d644759e67ebfc29caf9da751cc974e11b08
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-08-31 15:53:04 +0530

    Update 1-Nwithloop.cpp

--- 1-Nwithloop.cpp
+++ 1-Nwithloop.cpp
@@ -8,14 +8,14 @@ int main(){
     cin >> n; // Read the number input from the user
     int sum = 0; // Initialize a variable to store the sum, starting at 0
     // Loop from 1 to the input number (inclusive) to calculate the sum
-    for (int i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++) {
         sum += i; // Add the current value of `i` to the running total `sum`
     }
     // Display the final sum
     cout << "Sum from 1 to " << n << " is " << sum;
     return 0; // Indicate successful program termination
 }
+
 
 
 
