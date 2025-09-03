868210e7408a100f62da90bcb94fac0068ccec27
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-09-03 12:45:48 +0530

    Update Kth Largest Element in an Array.cpp

--- Kth Largest Element in an Array.cpp
+++ Kth Largest Element in an Array.cpp
@@ -27,11 +27,11 @@ int main() {
     vector<int> nums = {3, 2, 1, 5, 6, 4};
     int k = 2;
     int result = sol.findKthLargest(nums, k);
-
     cout << "The " << k << "th largest element is: " << result << endl;
 
     return 0;
 }
+
 
 
 
