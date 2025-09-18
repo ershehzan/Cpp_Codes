55ba2519400ddb5dd97cbd424eef5b2ed333fbba
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-09-18 18:34:27 +0530

    Update Build Min-Heap.cpp

--- Build Min-Heap.cpp
+++ Build Min-Heap.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 // Function to maintain the min heap property for a subtree rooted at index 'i'.
 // 'n' is the total number of elements in the heap (array).
 void heapify(vector<int>& arr, int i, int n)
@@ -62,3 +61,4 @@ int main()
 
     return 0;
 }
+
