5f5be645d4b9be0abdd5e7ae6f36c4bed713f989
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-09-19 18:01:01 +0530

    Update Deletion at end.cpp

--- Deletion at end.cpp
+++ Deletion at end.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
     int n; // Variable to store the number of elements in the array
     cout << "Enter the number of elements in the array: ";
     cin >> n; // Read the number of elements from the user
@@ -26,4 +25,5 @@ int main()
     cout << endl; // Print a newline character at the end
 
     return 0; // Return 0 to indicate successful execution
+
 }
