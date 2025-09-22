3e3524a4472045b7db249dba04bda8e9c7bf9b5b
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-09-22 18:38:05 +0530

    Update 2D vector.cpp

--- 2D vector.cpp
+++ 2D vector.cpp
@@ -23,7 +23,6 @@ for(int i=0;i<n;i++){
         cin>>matrix[i][j];
     }
 }
-
 for(int i=0;i<n;i++){
     for(int j=0;j<m;j++){
         cout<<matrix[i][j]<<" ";
@@ -32,6 +31,7 @@ for(int i=0;i<n;i++){
 cout<<endl;
 
 }
+
 
 
 
