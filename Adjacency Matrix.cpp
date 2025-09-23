70039699ed0a118ed42953dc242def8070b82ea4
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-09-23 18:47:05 +0530

    Update Adjacency Matrix.cpp

--- Adjacency Matrix.cpp
+++ Adjacency Matrix.cpp
@@ -2,8 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 // Undirected Graph using Adjacency Matrix
-int main()
-{
+int main(){
     int vertex, edges;
     cout << "Enter number of vertices and edges: ";
     cin >> vertex>>edges;
@@ -27,4 +26,5 @@ int main()
         }
         cout<<endl;
     }
+
 }
